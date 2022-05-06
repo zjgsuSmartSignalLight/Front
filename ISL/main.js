28c55420945a83d49a16a334017fb512d7774837
闪亮亮的小逗比 <8284735+bright-little-dobby@user.noreply.gitee.com>
2022-05-06 09:57:44 +0800

    监测完成

--- ISL/main.js
+++ ISL/main.js
@@ -3,8 +3,25 @@ import App from './App'
 // #ifndef VUE3
 import Vue from 'vue'
 
+//导入页面组件
 import home from './pages/home/home.vue'
 Vue.component('home',home)
+
+import monitor from './pages/monitor/home.vue'
+Vue.component('monitor',monitor)
+
+import heatmap from "./pages/monitor/heatmap.vue"
+Vue.component('heatmap',heatmap)
+
+import pointMonitor from "./pages/monitor/pointMonitor.vue"
+Vue.component('pointMonitor',pointMonitor)
+
+import violation from "./pages/monitor/violation.vue"
+Vue.component('violation',violation)
+
+import viodetail from "./pages/monitor/violationDetail.vue"
+Vue.component('viodetail',viodetail)
+
 
 
 Vue.config.productionTip = false
